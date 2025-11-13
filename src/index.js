0c2409c302f8f7462065a5bdf4befcf32130f8f0
dongha <donghak937@gmail.com>
2025-11-13 12:15:00 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const myElement = <div style={{backgroundColor : "red"}}>I Love JSX <div>HaHa</div></div>; // JSX
